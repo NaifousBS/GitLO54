7d8a0337efdbd46eb278fa0ebba789244f186192
Ahmet01100 <ahmet.imre@hotmail.fr>
2016-12-02 17:15:08 +0100

    Communication avec la BDD OK

--- projetLO54/src/main/java/fr/utbm/main/AppGetLocation.java
+++ projetLO54/src/main/java/fr/utbm/main/AppGetLocation.java
@@ -7,7 +7,7 @@ public class AppGetLocation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DefaultLocationController defLocationController=new DefaultLocationController();
-		defLocationController.getLocation(1);
+                System.out.println(defLocationController.getLocation(1).getCity());
 	}
 
 }
